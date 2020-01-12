fd55a6d984b44949aad0ff48d1a7ac2ef0efe2f5
ronger <ronger-x@outlook.com>
2020-01-12 18:30:36 +0800

    :tada: first commit

---
+++ src/test/java/com/rymcu/activitidemo/ActivitiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rymcu.activitidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
